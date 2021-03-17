fcbf44dca78c3a16bbae4c31197adeeb5274d650
RohitKuwar <70262783+RohitKuwar@users.noreply.github.com>
2021-03-17 14:22:36 +0530

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,30 +2,46 @@ import React, { useEffect, useState } from 'react';
 import styles from './style.module.css';
 
 export default function Calculator() {
-const [value1, setValue1] = useState();
-const [value2, setValue2] = useState();
-const [operation, setOperation] = useState();
+const [value1, setValue1] = useState('');
+const [value2, setValue2] = useState('');
+const [operation, setOperation] = useState('+');
 const [result, setResult] = useState();
 
 const clearInputs = () => {
   setValue1('');
   setValue2('');
+  setOperation('+');
 }
 
 useEffect(()=>{
-  if(operation === '+'){
-    setResult(value1 + value2);
+  switch (operation) {
+    case '+':
+      setResult(Number(value1) + Number(value2));
+      break;
+    case '-':
+      setResult(value1 - value2);
+      break;
+    case '*':
+      setResult(value1 * value2);
+      break;
+    case '/':
+      setResult((value1 / value2).toFixed(2));
+      break;
+    default:
+      setResult(null);
   }
-},[operation, value1, value2])
+}, [operation, value1, value2])
 
   return (
     <>
     <header>CALCULATOR</header>
     <div className={styles.container}>
       <div className={styles.inputFields}>
-        <input type="text" value={value1} onChange={(event)=>event.target.value} />
+        <input type="number" value={value1} placeholder="Eg: 1" autoComplete="off"
+        onChange={(event)=>setValue1(event.target.value)} />
         <label htmlFor="">{operation}</label>
-        <input type="text" value={value2} onChange={(event)=>event.target.value} />
+        <input type="number" value={value2} placeholder="Eg: 2" autoComplete="off"
+        onChange={(event)=>setValue2(event.target.value)} />
       </div>
       <div className={styles.buttons}>
         <button className={styles.btn} onClick={()=>setOperation('+')}>+</button>
