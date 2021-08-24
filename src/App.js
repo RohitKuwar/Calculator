e1d52567f5f3bac0a77723bfb9ac070af147b418
RohitKuwar <rohit1kuwar@gmail.com>
2021-08-24 20:55:31 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Calculator from './Calculator';
+import Footer from './footer/Footer';
 
 function App() {
   return (
     <div>
       <Calculator />
+      <Footer />
     </div>
   );
 }
---
+++ src/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.module.css';
+
+export default function footer() {
+  return (
+    <footer>
+      <a href="https://github.com/RohitKuwar/Calculator" target="_blank">
+        Click here for GitHub repository link
+      </a>
+    </footer>
+  )
+}
